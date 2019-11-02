1aab2073e9f9e9e9bc68aea18b04e2f136a7b93b
Kristin Håberg <krishaa@stud.ntnu.no>
2019-11-02 16:26:27 +0100

    med meny

--- js/domino.js
+++ js/domino.js
@@ -3,7 +3,7 @@ let img=document.getElementById("teamgroups").querySelectorAll(".personbox");
 
 function domino(){
   for (i=0; i<img.length; i++){
-    delay=i * 0.25;
+    delay=i * 0.1;
     img[i].style.animation="domino 0.8s " + delay + "s ease-in";
   }
 }
