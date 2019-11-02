accf3147848c8cddc92f8e700a3ee47047a04bd8
Kari Stam <karif@live.no>
2019-11-02 12:44:34 +0100

    fixing nav table

---
+++ js/table.js
@@ -0,0 +1,4 @@
+const scientific=document.getElementsByClass("scientific");
+const chapters=document.getElementsByClass("chapters");
+const books=document.getElementsByClass("books");
+const scientific=document.getElementsByClass("reports");
