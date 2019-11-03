cefab31827f936c8c173d14b652c912a0f0ee4d9
Emil Brevik <emil.brevik@gmail.com>
2019-11-03 18:58:18 +0100

    flexing random shit

--- js/header+footer.js
+++ js/header+footer.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(e){
   const menu=document.getElementById("menu");
-        menu.innerHTML=`<div id="navwrap">`
-            +`<div class="navbar bodytext1">`
-              +`<a href="index.html" id="homeButton"></a>`
-              +`<a href="about.html">About</a>`
-              +`<a href="people.html">People</a>`
-              +`<a href="publications.html">Publications</a>`
-              +`<a href="events.html">Events</a>`
-              +`<a href="news.html">News</a>`
-              +`<div id="searchBox">`
-                +`<input type="text" placeholder="Search.." id="searchText">`
-                +`<input type="submit" value="" id="searchButton">`
+        menu.innerHTML='<div id="navwrap">'
+            +'<div class="navbar bodytext1">'
+              +'<a href="index.html" id="homeButton"></a>'
+              +'<a href="about.html">About</a>'
+              +'<a href="people.html">People</a>'
+              +'<a href="publications.html">Publications</a>'
+              +'<a href="events.html">Events</a>'
+              +'<a href="news.html">News</a>'
+              +'<div id="searchBox">'
+                +'<input type="text" placeholder="Search.." id="searchText">'
+                +'<input type="submit" value="" id="searchButton">'
               +"</div>"
             +"</div>"
         +"</div>"
   const footer=document.getElementById("footer");
-        footer.innerHTML=`<div id="footwrap">`
+        footer.innerHTML='<div id="footwrap">'
         +'<img id="footerImg" src="./img/Logo_text.png"></img>'
         +'<div id="contactText">'
           +'<h3 class="footerH">Postal adress</h3>'
