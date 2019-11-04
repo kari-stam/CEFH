6429e15960b3c56a82866e63a9950163b55b6af4
Emil Brevik <emil.brevik@gmail.com>
2019-11-04 14:53:27 +0100

    dropbox furteghr

--- js/collapsible.js
+++ js/collapsible.js
@@ -3,15 +3,30 @@ var drop = document.getElementsByClassName("dropdown");
 
 var i;
 
-for (i=0; i < drop.length; i++){
+/*for (i=0; i < drop.length; i++){
     drop[i].addEventListener('click', function(){
         this.classList.toggle('active');
         var content = this.nextElementSibling;
-        if (content.style.display === 'flex'){
-            content.style.display = 'none';
-        }
-        else{
+        if (content.style.display === 'none'){
             content.style.display = 'flex';
         }
+        else{
+            content.style.display = 'none';
+        }
     });
-}
+}*/
+
+for (i = 0; i < drop.length; i++) {
+    var cont = document.getElementsByClassName('dropCont')[i].getElementsByClassName('newsArticle');
+    console.log(cont);
+    drop[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } 
+        else {
+            content.style.maxHeight = 250*cont.length + 'px';
+        }
+    });
+  }
