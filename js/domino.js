d1a4e48a6848de33d360a2de31f56a992cc77dc9
Kristin Håberg <krishaa@stud.ntnu.no>
2019-11-05 15:55:35 +0100

    tryingtochangeindex javascirpt

--- js/domino.js
+++ js/domino.js
@@ -1,5 +1,5 @@
 
-let img=document.getElementById("teamgroups").querySelectorAll(".personbox");
+/*let img=document.getElementById("teamgroups").querySelectorAll(".personbox");
 
 function domino(){
   for (i=0; i<img.length; i++){
@@ -8,4 +8,26 @@ function domino(){
   }
 }
 
-domino();
+domino();*/
+
+
+let img=document.getElementById("onOneRow_rec").querySelectorAll(".pubBox");
+
+function domino(){
+  if (y=917) {
+    for (i=0; i<img.length; i++){
+      delay=i * 0.2;
+      img[i].style.animation="domino 0.8s " + delay + "s ease-in";
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  //let y= window.pageYOffset;
+  let y= window.scrollY;
+  let x= 917; //position of pubBox
+  console.log(y);
+  if (y==x) {
+    domino();
+  }
+});
