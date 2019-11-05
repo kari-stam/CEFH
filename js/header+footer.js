ff772263e08b3ad4419a0bd6982dc112c649d440
Kristin Håberg <krishaa@stud.ntnu.no>
2019-11-05 13:58:49 +0100

    med responsive navbar

--- js/header+footer.js
+++ js/header+footer.js
@@ -1,3 +1,5 @@
+// header and footer inspired from: https://github.com/aslakhol/saftogsuse/blob/master/scripts/add_header_footer.js
+
 document.addEventListener("DOMContentLoaded", function(e){
   const menu=document.getElementById("menu");
         menu.innerHTML='<div id="navwrap">'
@@ -12,6 +14,18 @@ document.addEventListener("DOMContentLoaded", function(e){
                 +'<input type="text" placeholder="Search.." id="searchText">'
                 +'<input type="submit" value="" id="searchButton">'
               +"</div>"
+              + '<button class="navbar-toggle" onclick="dropdown()">'
+                  +'<img src="img/hamburger.svg" alt="hamburger menu">'
+              +'</button>'
+              +'<div class="dropdownmenu">'
+                +'<ul>'
+                  +'<li><a href="about.html">About</a></li>'
+                  +'<li><a href="people.html">People</a></li>'
+                  +'<li><a href="publications.html">Publications</a></li>'
+                  +'<li><a href="events.html">Events</a></li>'
+                  +'<li><a href="news.html">News</a></li>'
+                +'</ul>'
+                +'</div>'
             +"</div>"
         +"</div>"
   const footer=document.getElementById("footer");
@@ -30,3 +44,24 @@ document.addEventListener("DOMContentLoaded", function(e){
           +'<p class="footerP">Marcus Thranes gate 2, Oslo</p>'
         +'</div> '
 });
+
+
+
+// functions inspired by w3schools: https://www.w3schools.com/howto/howto_js_dropdown.asp
+//functions for the dropdown menu
+
+/*when you click the button the dropdownmenu is added the class shown (styling in css)*/
+function dropdown(){
+  document.querySelector(".dropdownmenu").classList.toggle("show");
+}
+
+/*when you click on the page->
+        if you click on the hamburgerbutton it checks if the dropdown contains class shown or not, and toggles thereafter*/
+window.onclick = function(event) {
+  if (!event.target.matches('.navbar-toggle')) {
+    const dropdown = document.querySelector(".dropdownmenu");
+      if (dropdown.classList.contains('show')) {
+        dropdown.classList.remove('show');
+      }
+    }
+  }
