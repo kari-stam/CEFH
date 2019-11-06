f138b8cc4b35e69d554ce0c9bf08d9eef48afd0b
Oda <oda.asheim@gmail.com>
2019-11-06 16:04:01 +0100

    Update dropdownevents.js
    working for november

--- js/dropdownevents.js
+++ js/dropdownevents.js
@@ -1,13 +1,13 @@
 
 
-/*js for dropwoen menus*/
-let drop = document.getElementsByClassName("dropdown");
+var drop = document.getElementsByClassName("dropdown");
 
 var i;
 
+
 for (i = 0; i < drop.length; i++) {
     let dropCont=document.getElementsByClassName('monthEvents');
-    let cont = document.getElementsByClassName('dropCont')[i].getElementsByClassName('event');
+    let cont = document.getElementsByClassName('monthEvents')[i].getElementsByClassName('event');
     console.log(cont);
     drop[i].addEventListener("click", function() {
         this.classList.toggle("active");
